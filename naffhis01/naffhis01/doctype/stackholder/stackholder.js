a9705672e86949dd20e008f94d46d55780c40cfd
nayan441 <yadavnayan441@gmail.com>
2022-06-14 18:18:28 +0530

    first commit

---
+++ naffhis01/naffhis01/doctype/stackholder/stackholder.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, nayan and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Stackholder', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ naffhis01/naffhis01/doctype/stackholder/test_stackholder.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, nayan and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestStackholder(unittest.TestCase):
+	pass
