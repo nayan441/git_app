e412ba6d63ffd4d2cf096fde00b2522c1d334c03
nayan441 <yadavnayan441@gmail.com>
2022-06-20 13:41:39 +0530

    All job added02

--- naffhis01/tasks.py
+++ naffhis01/tasks.py
@@ -1,4 +1,14 @@
 import frappe
+def all():
+    new_doc = frappe.get_doc(
+        {
+            "doctype" : "Note",
+            "title" : "Hello world fro all"
+        }
+    )
+
+    new_doc.insert()
+    frappe.db.commit()
 
 def cron():
     new_doc = frappe.get_doc(
